c857986985832b145f3e9f55473f01fa280c9888
pc24495 <pchummar42@gmail.com>
2022-03-16 17:28:52 -0400

    Added getStaticProps to about page, testing that it works in production

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,20 @@
 import classes from "../styles/About.module.css";
 
-export default function About() {
+export default function About({ title, ...props }) {
   return (
     <div className={classes.About}>
       <h1>Hey</h1>
+      <p>{title}</p>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      title: "Prajwal's Test Blog",
+    },
+  };
 }
