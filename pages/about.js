b3901952df51c2f9fd5b104851ef86430498428a
pc24495 <pchummar42@gmail.com>
2022-05-05 14:21:15 -0400

    Fixed about page to remove database-related code

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,13 @@
 import classes from "../styles/About.module.css";
 import { useUsername } from "../store/UsernameProvider/UsernameProvider.js";
 
-export default function About({ title, users, ...props }) {
+export default function About({ title, ...props }) {
   const { username } = useUsername();
 
   return (
     <div className={classes.About}>
       <h1>Hey</h1>
       <p>{title}</p>
-      <p>{username}</p>
-      {users.map((user) => (
-        <p>{user.username}</p>
-      ))}
     </div>
   );
 }
@@ -19,17 +15,10 @@ export default function About({ title, users, ...props }) {
 export async function getStaticProps() {
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
-  const dateStripped = require("../helpers/dateStripped.js");
-  const Users = require("../database/models/Users.js");
-
-  let users = await Users.query().withGraphFetched("products");
-
-  users = dateStripped(users);
 
   return {
     props: {
       title: "Prajwal's Awesome Test Blog",
-      users,
     },
   };
 }
